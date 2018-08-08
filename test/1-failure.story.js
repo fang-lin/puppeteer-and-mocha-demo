65cd3a9d5ee06b8be8203cdabb9fac21f84233e0
Lin Fang <fang.lin@rea-group.com>
2018-08-08 21:47:29 +0800

    Add blank files

---
+++ test/1-failure.story.js
@@ -0,0 +1,6 @@
+describe('sample test', function () {
+  it('should work', async function () {
+    console.log(await browser.version());
+    expect(true).to.be.true;
+  });
+});
--- test/tests.js
+++ test/tests.js
@@ -1,9 +1,9 @@
 const { expect } = require('chai');
 const puppeteer = require('puppeteer');
+const _ = require('lodash');
 const globalVariables = _.pick(global, ['browser', 'expect']);
 
 // puppeteer options
-
 const opts = {
   headless: false,
   slowMo: 100,
@@ -11,9 +11,15 @@ const opts = {
 };
 
 // expose variables
-
-before(async function () {
+before(async () => {
   global.expect = expect;
   global.browser = await puppeteer.launch(opts);
 });
 
+// close browser and reset global variables
+after(() => {
+  browser.close();
+
+  global.browser = globalVariables.browser;
+  global.expect = globalVariables.expect;
+});
